8345470924e45e3b79bacd840cdabd0640076c79
Vinh Nguyen <nguyenvq714@gmail.com>
2022-08-15 06:10:19 +0000

    version 0.0.2

---
+++ R/data_ccc_list.R
@@ -0,0 +1,20 @@
+#' California Community Colleges address data set
+#'
+#' List of California Community Colleges (CCC) and their addresses, obtained from \href{https://www.cccco.edu/Students/Find-a-College/College-Alphabetical-Listing}{this} site on August 31, 2021.
+#' @format A data frame with the following variables:
+#' \describe{
+#'   \item{College}{College name.}
+#'   \item{District}{District name.}
+#'   \item{Address}{Address of the college.}
+#'   \item{Phone}{Phone number of the college.}
+#'   \item{Website}{Web address of the college.}
+#' }
+#' @docType data
+#' 
+#' @usage data(ccc_list)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(ccc_list)
+"ccc_list"
---
+++ R/uc_list.R
@@ -0,0 +1,17 @@
+#' University of California address data set
+#'
+#' List of Universities of California (UC) and their addresses, gathered on September 1, 2021.
+#' @format A data frame with the following variables:
+#' \describe{
+#'   \item{University}{University name.}
+#'   \item{Address}{Address of the university.}
+#' }
+#' @docType data
+#' 
+#' @usage data(uc_list)
+#'
+#' @keywords datasets
+#'
+#' @examples
+#' data(uc_list)
+"uc_list"
