2d941f1051f6ee054bed9a2cc770d5f181378fb9
Vinh Nguyen <nguyenvq714@gmail.com>
2023-06-07 17:40:02 +0000

    version 0.0.3

---
+++ inst/doc/Ex-05-Geocoding-Addresses.R
@@ -0,0 +1,3 @@
+## ---- include=FALSE-----------------------------------------------------------
+knitr::opts_chunk$set(error=TRUE) # in case CRAN vignette build does not have internet, fail gracefully
+
